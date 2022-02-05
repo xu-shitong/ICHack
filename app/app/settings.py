68c7e084e447e21f261c9e876bbc81809d4822ad
Kenton Kwok <54623370+kwokkenton@users.noreply.github.com>
2022-02-05 23:09:05 +0000

    add home page

--- app/app/settings.py
+++ app/app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'voice.apps.VoiceConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
