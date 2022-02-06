fa8dfc20e66d23a1eb41fbea0a4b22ec373b4551
Ragviswa <rv519@ic.ac.uk>
2022-02-06 07:46:00 +0000

    Adding Better Prints

--- speech_to_text/Audio_into_Words.py
+++ speech_to_text/Audio_into_Words.py
@@ -6,7 +6,7 @@ from google.cloud import speech_v1 as speech
 from google.cloud import storage
 
 def split_audio_transcript_to_words(audio_path, cleandir=False):
-    config = speech.RecognitionConfig(sample_rate_hertz = 44100, audio_channel_count = 2, language_code="en-US", enable_word_time_offsets=True,)
+    config = speech.RecognitionConfig(sample_rate_hertz = 48000, audio_channel_count = 2, language_code="en-US", enable_word_time_offsets=True,)
 
     audio_data = AudioSegment.from_wav(audio_path)
     sentences_audio = split_on_silence(audio_segment=audio_data, min_silence_len=500, silence_thresh=-35, keep_silence=1000)
@@ -43,7 +43,6 @@ def split_audio_transcript_to_words(audio_path, cleandir=False):
         word_list = speech_to_word_list(config, sentence_heard)
         
         for word, starttime, endtime in word_list:
-            print(starttime, " | ", endtime)
             starttime=starttime*1000
             endtime=endtime*1000
             word_audio = sentence_audio[starttime:endtime]
@@ -56,20 +55,19 @@ def speech_to_word_list(config, audio):
     client = speech.SpeechClient()
     response = client.recognize(config=config, audio=audio)
     word_list = []
-    print(response.results)
     for result in response.results:
         best_alternative = result.alternatives[0]
-        print(best_alternative.transcript)
         for word in best_alternative.words:
             start_s = word.start_time.total_seconds()
             end_s = word.end_time.total_seconds()
             word = word.word
             word_list.append((word, start_s, end_s))
+            print(word, " | ", start_s, "->", end_s)
         return word_list
     return word_list
 
 def main():
-    audio_path = "obamashort.wav"
+    audio_path = "DonaldTrumpShort.wav"
     split_audio_transcript_to_words(audio_path, cleandir=True)
     
 if __name__ == "__main__":
