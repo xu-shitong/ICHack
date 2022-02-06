92ff5b05563c6c5da79c5b6749a7cabbd854fa5d
Ragviswa <rv519@ic.ac.uk>
2022-02-06 07:09:25 +0000

    Fixed Audio_to_Words.py

--- speech_to_text/Audio_into_Words.py
+++ speech_to_text/Audio_into_Words.py
@@ -3,10 +3,10 @@ import shutil
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
 from google.cloud import speech_v1 as speech
-import base64
+from google.cloud import storage
 
 def split_audio_transcript_to_words(audio_path, cleandir=False):
-    config = dict(language_code="en-US", sample_rate_hertz=44100, enable_word_time_offsets=True)
+    config = speech.RecognitionConfig(sample_rate_hertz = 44100, audio_channel_count = 2, language_code="en-US", enable_word_time_offsets=True,)
 
     audio_data = AudioSegment.from_wav(audio_path)
     sentences_audio = split_on_silence(audio_segment=audio_data, min_silence_len=500, silence_thresh=-35, keep_silence=1000)
@@ -32,14 +32,22 @@ def split_audio_transcript_to_words(audio_path, cleandir=False):
         sentence_audio_filepath = os.path.join(sentences_folder, f"sentence{i}.wav")
         sentence_audio.export(sentence_audio_filepath, format="wav")
 
-        enc = base64.b64encode(open(sentence_audio_filepath, "rb").read())
-        sentence_heard = dict(content=enc)
+        storage_client = storage.Client()
+
+        bucket = storage_client.get_bucket('sentence_audios') 
+        file_neme = f"sentence{i}.wav"
+        blob = bucket.blob(file_neme)
+        blob.upload_from_filename(sentence_audio_filepath)
+
+        sentence_heard = speech.RecognitionAudio(uri="gs://sentence_audios/"+file_neme,)
         word_list = speech_to_word_list(config, sentence_heard)
         
         for word, starttime, endtime in word_list:
+            print(starttime, " | ", endtime)
             starttime=starttime*1000
             endtime=endtime*1000
             word_audio = sentence_audio[starttime:endtime]
+            word = word.lower()
             word_audio_filepath = os.path.join(words_folder, f"{word}.wav")
             if not os.path.isfile(word_audio_filepath):
                 word_audio.export(word_audio_filepath, format="wav")
@@ -56,7 +64,7 @@ def speech_to_word_list(config, audio):
             start_s = word.start_time.total_seconds()
             end_s = word.end_time.total_seconds()
             word = word.word
-            word_list.append(word, start_s, end_s) 
+            word_list.append((word, start_s, end_s))
         return word_list
     return word_list
 
