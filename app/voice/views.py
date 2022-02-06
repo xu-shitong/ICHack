293516543820171d8c5d499e5c8bcbdfd1845684
cluelesselectrostar <59958064+cluelesselectrostar@users.noreply.github.com>
2022-02-06 02:04:20 +0000

    audio playback

--- app/voice/views.py
+++ app/voice/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
 # Create your views here.
 
+from pathlib import Path
+import os
+
 def home(request):
     if request.method == 'POST':
         uploaded_file = request.FILES['track']
         fs = FileSystemStorage()
         name = fs.save(uploaded_file.name, uploaded_file)
         url = fs.url(name)
+        base_url = './../../..' + url
         print(uploaded_file.name)
         print(url)
-    return render(request, 'voice/home.html')
+        print(base_url)
+    return render(request, 'voice/home.html', {"file":base_url})
