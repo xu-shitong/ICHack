cb2ff07bd9e6ec92dcaaee5d52e450cd820163de
Kenton Kwok <54623370+kwokkenton@users.noreply.github.com>
2022-02-06 00:50:01 +0000

    Added upload function

--- app/voice/views.py
+++ app/voice/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
+    if request.method == 'POST':
+        uploaded_file = request.FILES['track']
+        print(uploaded_file.name)
     return render(request, 'voice/home.html')
