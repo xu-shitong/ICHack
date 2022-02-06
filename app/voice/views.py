43c1316efc2f3c12f45aced5e2250ad229869f21
Kenton Kwok <54623370+kwokkenton@users.noreply.github.com>
2022-02-06 01:25:39 +0000

    upload save

--- app/voice/views.py
+++ app/voice/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-
+from django.core.files.storage import FileSystemStorage
 # Create your views here.
 
 def home(request):
     if request.method == 'POST':
         uploaded_file = request.FILES['track']
+        fs = FileSystemStorage()
+        name = fs.save(uploaded_file.name, uploaded_file)
+        url = fs.url(name)
         print(uploaded_file.name)
+        print(url)
     return render(request, 'voice/home.html')
